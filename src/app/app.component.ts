c877bdad721af30fdaf12dc2b6eecfbba9be2ccf
Jayesh Panchal <jayesh@forkmedia.in>
2018-05-18 16:38:09 +0530

    Update demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  open = false;
+  position = 'right';
+  showCloseButton = true;
+  showBackdrop = true;
+  hideOnBackdropClick = true;
+
+  togglePosition() {
+    if (this.position === 'right') {
+      this.position = 'bottom';
+    } else if (this.position === 'bottom') {
+      this.position = 'left';
+    } else if (this.position === 'left') {
+      this.position = 'top';
+    } else if (this.position === 'top') {
+      this.position = 'right';
+    }
+  }
 }
